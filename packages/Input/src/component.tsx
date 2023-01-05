d93718b9eb400a1d11aa7cd0d198b9015cc67a63
heyu <1395294694@qq.com>
2023-01-05 20:58:10 +0800

    feat: Input 支持 ref

--- packages/Input/src/component.tsx
+++ packages/Input/src/component.tsx
@@ -20,15 +20,15 @@ interface Props {
      size?: 's' | 'm' | 'l'
 }
 
-export default function (props: Props) {
+export default React.forwardRef(function (props: Props, ref: React.ForwardedRef<HTMLDivElement>) {
     const onInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (props.onChange) {
             props.onChange(event.target.value)
         }
     }
 
-    return <div className={style.vitisInput}>
+    return <div className={style.vitisInput} ref={ref}>
             {props.label && <span className={style.label}>{props.label}</span>}
             <input value={props.value} onInput={onInput}/>
     </div>
-}
+})
